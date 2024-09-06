71a958d461989221e5e75979d524f00d23073ba4
kamilraza-expertizo <kamil.raza@expertizo.pk>
2024-09-06 15:45:46 +0500

    first commit

--- src/components/StaticRating.jsx
+++ src/components/StaticRating.jsx
@@ -5,18 +5,6 @@ import { FaRegStar } from "react-icons/fa6";
 
 const Rating = ({ difficulty }) => {
 
-  const difficultyObj = {
-    hard: 3,
-    medium: 2,
-    easy: 1
-  }
-
-  const solidStars = difficultyObj[difficulty] || 0;
-  const stars = [
-    ...Array(solidStars).fill(<LiaStarSolid />),
-    ...Array(5 - solidStars).fill(<FaRegStar />),
-  ];
-
   let starComponent = <></>
   switch (difficulty) {
     case 'hard':
